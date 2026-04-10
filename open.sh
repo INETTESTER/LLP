afa258ca831dd40a59f942de86cc21edd11af60b
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-04-10 11:20:48 +0700

    update

--- open.sh
+++ open.sh
@@ -6,8 +6,8 @@
 ##########################################################################
                      API="withdrawal" 
                      google_sheet="https://docs.google.com/spreadsheets/d/1lmHM0MZmz3KO7-UHtuym-aKtLy5e5KWzYLuUB2AGE7k/edit?gid=158997130#gid=158997130" 
-                     id="7"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="17000";            #จำนวนผู้ใช้งาน
+                     id="8"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     user="12500";            #จำนวนผู้ใช้งาน
                      duration="180";         #วินาที
                      scenario="3"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
