523aa1d168d4a9911c9b1f4e53087d0852d9a8a4
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-04-10 11:12:51 +0700

    update

--- open.sh
+++ open.sh
@@ -6,9 +6,9 @@
 ##########################################################################
                      API="withdrawal" 
                      google_sheet="https://docs.google.com/spreadsheets/d/1lmHM0MZmz3KO7-UHtuym-aKtLy5e5KWzYLuUB2AGE7k/edit?gid=158997130#gid=158997130" 
-                     id="6"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="10000";            #จำนวนผู้ใช้งาน
-                     duration="300";         #วินาที
+                     id="7"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     user="17000";            #จำนวนผู้ใช้งาน
+                     duration="180";         #วินาที
                      scenario="3"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
                                             #scenario="3" ยิงเเบบกำหนด request แต่ไม่แม่นยำ (duration กี่วินาทีก็ได้)
